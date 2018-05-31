7bd17545cb0d8a2fb8889ee6cb9fc37a8a11da9f
Andrew Wei <andrewscwei@gmail.com>
2018-05-31 02:20:24 -0700

    Updates

--- utils/routes.js
+++ utils/routes.js
@@ -13,7 +13,7 @@ exports.parseDir = function(dir, baseDir = dir) {
   pages.forEach(filename => {
     if ((/(^|\/)\.[^/.]/g).test(filename)) return;
 
-    const basename = path.basename(filename, `.jsx`);
+    const basename = path.basename(filename, `.js`);
 
     // Ignore files with certain names (i.e. 404.vue). No need to generate a
     // route for these files.

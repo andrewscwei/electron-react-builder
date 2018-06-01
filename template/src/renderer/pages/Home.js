55728a92f31e2afd817eba0c2ce9c97d106c641d
Andrew Wei <andrewscwei@gmail.com>
2018-06-01 00:51:48 -0700

    Update Home

--- template/src/renderer/pages/Home.js
+++ template/src/renderer/pages/Home.js
@@ -1,12 +1,12 @@
 import Logo from '@/components/Logo';
 import { incrementOnce, incrementTwice } from '@/store/counter';
+import { changeLocale } from '@/store/i18n';
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
+import { FormattedHTMLMessage } from 'react-intl';
 
 const StyledLogo = styled(Logo)`
   margin-bottom: 50px;
@@ -43,6 +43,10 @@ const Root = styled.div`
     font-weight: 600;
     margin: 0 0 20px;
     text-align: center;
+
+    em {
+      opacity: .2;
+    }
   }
 
   p {
@@ -53,6 +57,7 @@ const Root = styled.div`
 
     em {
       color: ${props => props.theme.accentColor};
+      font-weight: 600;
     }
   }
 
@@ -78,7 +83,7 @@ const Root = styled.div`
 `;
 
 const mapStateToProps = (state) => ({ locale: state.i18n.locale, t: state.i18n.messages, count: state.counter.count });
-const mapDispatchToProps = (dispatch) => bindActionCreators({ incrementOnce, incrementTwice }, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({ changeLocale, incrementOnce, incrementTwice }, dispatch);
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class Home extends PureComponent {
@@ -86,6 +91,7 @@ export default class Home extends PureComponent {
     locale: PropTypes.string.isRequired,
     t: PropTypes.object.isRequired,
     count: PropTypes.number.isRequired,
+    changeLocale: PropTypes.func.isRequired,
     incrementOnce: PropTypes.func.isRequired,
     incrementTwice: PropTypes.func.isRequired,
   }
@@ -94,26 +100,29 @@ export default class Home extends PureComponent {
     t: {},
   }
 
+  toggleLocale = () => {
+    if (this.props.locale === `en`) {
+      this.props.changeLocale(`ja`);
+    }
+    else {
+      this.props.changeLocale(`en`);
+    }
+  }
+
   render() {
-    const { locale, t, count, incrementOnce, incrementTwice } = this.props;
+    const { t, count, incrementOnce, incrementTwice } = this.props;
 
     return (
       <Root>
         <StyledLogo/>
         <summary>
           <h1>
-            <FormattedMessage
-              id='hello'
-              values={{
-                count: count,
-                count2: count*2,
-              }}
-            />
+            <FormattedHTMLMessage id='hello' values={{ count: count, count2: count*2 }}/>
           </h1>
           <p dangerouslySetInnerHTML={{ __html: t[`description`] }}/>
         </summary>
         <nav>
-          <Link to={locale === `en` ? `/jp` : `/`}>{t[`switch-lang`]}</Link>
+          <button onClick={this.toggleLocale}>{t[`switch-lang`]}</button>
           <button onClick={incrementOnce}>{t[`increment`]}</button>
           <button onClick={incrementTwice}>{t[`increment-twice`]}</button>
         </nav>
